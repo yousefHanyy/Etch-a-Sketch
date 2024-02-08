7197c6d52a2e1a6d9a2b8f2aacded1683117aeb0
Yousef Hany <yousefhanymahmoud72@gmail.com>
2024-02-08 09:59:01 +0200

    -Added a lot more styling to the slider. -The slider value is now connected to its position.(default:16)

--- script.js
+++ script.js
@@ -9,6 +9,16 @@
  *  todo|2-Based on the color we choose using the color picker, there should be an active class corresponding to that color so we can later use it to color the canvas.
  *
  */
+/**
+ *! Adjusting the slider should change the following:
+ *!  |1-The no. displayed above the slider (16x16).
+ **     |The 'sizeSlider' should be in relation to the 'sizeValue'.
+ **       |1-Define a constant for the sizeSlider and sizeValue elements.
+ **       |2-Get the value of the sizeSlider depending on its position.
+ **       |3-Change the displayed number by altering the sizeValue element depending on the new value of the sizeSlider.
+ *!  |2-The no. of boxes used in the canvas should be the product of the numbers chosen.
+ */
+
 const colorBtn = document.querySelector("#colorBtn");
 const eraserBtn = document.querySelector("#eraserBtn");
 const clearBtn = document.querySelector("#clearBtn");
@@ -22,3 +32,12 @@ function colorBtnCheck() {
 }
 colorBtn.addEventListener("click", colorBtnCheck);
 eraserBtn.addEventListener("click", eraserBtnCheck);
+
+const sizeSlider = document.querySelector("#sizeSlider");
+const sizeValue = document.querySelector("#sizeValue");
+
+sizeSlider.oninput = () => getSliderValue();
+function getSliderValue() {
+  let value = sizeSlider.value;
+  sizeValue.innerHTML = `${value} x ${value}`;
+}
